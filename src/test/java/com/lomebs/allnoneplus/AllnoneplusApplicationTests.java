51343981d12526bdaa0c7a97fab07114e057bf17
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2026-08-09 14:46:47 +0000

    init: initialize spring-boot web project with vaadin

---
+++ src/test/java/com/lomebs/allnoneplus/AllnoneplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lomebs.allnoneplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllnoneplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
